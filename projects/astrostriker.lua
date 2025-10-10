4563e61709b65ca36176e999cb1ed1cc71db866d
maxobur0001 <msblock09@gmail.com>
2025-10-10 18:36:18 +0300

    AstroStriker started (again yea)

--- projects/astrostriker.lua
+++ projects/astrostriker.lua
@@ -15,28 +15,31 @@ if SERVER then
 
     -- States
     local STATES = {
-        NotInUse = -1,
         Idle = 1
     }
 
+    ---@type Vehicle
     local seat = prop.createSeat(chip():getPos() + Vector(0, 0, 20), Angle(), "models/nova/airboat_seat.mdl")
     local size = Vector(80, 80, 20)
     local headsize = Vector(30, 30, 30)
     local body_hitbox = hitbox.cube(chip():getPos() + Vector(0, 0, -7.5), Angle(), size, true)
     local head_hitbox = hitbox.cube(chip():getPos() + Vector(0, 0, 60), Angle(), headsize, true)
-    local astro = AstroBase:new(STATES, body_hitbox, head_hitbox, seat, 65000)
+    local astro = AstroBase:new(body_hitbox, head_hitbox, seat, 65000)
 
     body.base[1]:setParent(body_hitbox)
+    body.base[2]:setParent(body.base[1])
     body.head[1]:setParent(head_hitbox)
     body.leftarm[1]:setParent(body.base[1])
     body.leftarm[2]:setParent(body.leftarm[1])
     body.rightarm[1]:setParent(body.base[1])
     body.rightarm[2]:setParent(body.rightarm[1])
 
-    body.leftarm[1]:setLocalAngles(Angle(40, -80, -120))
-    body.leftarm[2]:setLocalAngles(Angle(0, 0, 100))
-    body.rightarm[1]:setLocalAngles(Angle(40, 80, 120))
-    body.rightarm[2]:setLocalAngles(Angle(0, 0, -100))
+    body.base[2]:setLocalAngularVelocity(Angle(0, 200, 0))
+
+    body.leftarm[1]:setLocalAngles(Angle(40, 120, 120))
+    body.leftarm[2]:setLocalAngles(Angle(-100, 0, 0))
+    body.rightarm[1]:setLocalAngles(Angle(40, -120, -120))
+    body.rightarm[2]:setLocalAngles(Angle(-100, 0, 0))
 
 
     -- Movement think --
@@ -48,18 +51,10 @@ if SERVER then
     -- Idle animation
     local base_pos
     local head_pos
-    local leftarm_pos
-    local rightarm_pos
-    local leftarm2_ang
-    local rightarm2_ang
     IdleAnimation = FTimer:new(4, -1, {
         [0] = function()
             base_pos = body.base[1]:getLocalPos()
             head_pos = body.head[1]:getLocalPos()
-            leftarm_pos = body.leftarm[1]:getLocalPos()
-            rightarm_pos = body.rightarm[1]:getLocalPos()
-            leftarm2_ang = body.leftarm[2]:getLocalAngles()
-            rightarm2_ang = body.rightarm[2]:getLocalAngles()
         end,
         ["0-1"] = function(_, _, fraction)
             local rads = math.rad(360 * fraction)
@@ -67,41 +62,18 @@ if SERVER then
             local smoothed_y = math.cos(rads)
             body.base[1]:setLocalPos(base_pos + Vector(smoothed_x * 3, 0, smoothed_y * 3))
             body.head[1]:setLocalPos(head_pos + Vector(smoothed_x * 2, 0, smoothed_y * 2))
-            local smoothed_x_rev = (1 - smoothed_x)
-            local smoothed_y_rev = (1 - smoothed_y)
-            body.leftarm[1]:setLocalPos(leftarm_pos + Vector(smoothed_x_rev * 1, 0, smoothed_y_rev * 1))
-            body.rightarm[1]:setLocalPos(rightarm_pos + Vector(smoothed_x_rev * 1, 0, smoothed_y_rev * 1))
         end,
         ["0-0.5"] = function(_, _, fraction)
             local smoothed = math.easeInOutSine(fraction)
             body.base[1]:setLocalAngles(Angle(smoothed * 2, 0, 0))
             body.head[1]:setLocalAngles(Angle(smoothed * 3, 0, 0))
-            body.leftarm[2]:setLocalAngles(leftarm2_ang - Angle(0, 0, smoothed * 3))
-            body.rightarm[2]:setLocalAngles(rightarm2_ang + Angle(0, 0, smoothed * 3))
         end,
         ["0.5-1"] = function(_, _, fraction)
             local smoothed = math.easeInOutSine(1 - fraction)
             body.base[1]:setLocalAngles(Angle(smoothed * 2, 0, 0))
             body.head[1]:setLocalAngles(Angle(smoothed * 3, 0, 0))
-            body.leftarm[2]:setLocalAngles(leftarm2_ang - Angle(0, 0, smoothed * 3))
-            body.rightarm[2]:setLocalAngles(rightarm2_ang + Angle(0, 0, smoothed * 3))
         end
     })
-
-    -- On enter and leave --
-    hook.add("PlayerEnteredVehicle", "", function(ply, seat) astro:enter(ply, seat) end)
-    hook.add("PlayerLeaveVehicle", "", function(ply, seat) astro:leave(ply, seat) end)
-
-    -- On chip remove --
-    hook.add("Removed", "", function()
-        if seat and isValid(seat) then
-            local driver = seat:getDriver()
-            if isValid(driver) then
-                driver:setColor(Color(255, 255, 255, 255))
-            end
-        end
-    end)
-
 else
 
     local head
