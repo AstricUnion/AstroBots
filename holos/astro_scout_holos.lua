10901e906d92fbbd71916815ac1087ed8d9e79c1
C0nnector <voskresenskijaleksandr80@gmail.com>
2026-03-30 20:23:14 +0700

    Update astro_scout_holos.lua

--- holos/astro_scout_holos.lua
+++ holos/astro_scout_holos.lua
@@ -6,7 +6,7 @@ local holos = require("holos")
 ---@class Holo
 local Holo = holos.Holo
 local Rig = holos.Rig
-local SubHolo = holos.SubHolo
+local SubHolo = holos.SubHoloq
 
 local function circlePos(ang, radius, pos)
     local rad = math.rad(ang)
